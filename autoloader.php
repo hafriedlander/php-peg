b0ab22f64b7186bddd7b1c06922bbff898fba757
Johannes Künsebeck <jkuensebeck@taz.de>
2014-10-29 17:38:04 +0100

    Enabled composer autoloading

--- autoloader.php
+++ autoloader.php
@@ -1,6 +1,2 @@
 <?php
-
-require_once __DIR__.'/lib/vendor/SplClassLoader.php';
-
-$classLoader = new SplClassLoader('hafriedlander', __DIR__.'/lib');
-$classLoader->register();
+require_once 'vendor/autoload.php';
